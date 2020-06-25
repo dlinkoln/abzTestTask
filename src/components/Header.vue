d2eee74afed7164979cbb02c99dd531e3d61979b
dlinkoln <dlinkoln99@gmail.com>
2020-06-25 15:01:06 +0300

    prepeare cli | time 20min

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="header">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
